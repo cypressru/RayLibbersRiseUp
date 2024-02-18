20daeb5e8877cb319059f57a41892f94067f5a80
cypressru <140924725+cypressru@users.noreply.github.com>
2024-02-18 03:51:44 -0600

    Too much to describe

--- main.c
+++ main.c
@@ -8,6 +8,17 @@
 
 // include extra libraries here
 #include <raylib.h>
+#include <kos.h>
+#include <kos/init.h>
+#include <dc/sound/sound.h>
+#include <dc/sound/sfxmgr.h>
+#include <dc/maple.h>
+#include <dc/maple/controller.h>
+#include <dc/maple/vmu.h>
+#include <dc/fmath.h>
+#include <arch/arch.h>
+#include "dc_utils.h"
+#include "dc_utils.c"
 // -------
 
 //Useful values definitions
@@ -17,11 +28,16 @@
 #define BRICKS_LINES 5
 #define BRICKS_PER_LINE 20
 #define BRICKS_POSITION_Y 50
+#define LEFT 0
+#define CENTER 128
+#define RIGHT 255
+#define VMU_SCREEN_HEIGHT 32
+#define VMU_SCREEN_WIDTH 48
 //-----
 
 
 //Types and structures Definition
-typedef enum GameScreen { LOGO, TITLE, GAMEPLAY, ENDING, RAYLOGO } GameScreen; // Screen management
+typedef enum GameScreen { RAYLOGO, LOGO, TITLE, GAMEPLAY, ENDING } GameScreen; // Screen management
 
 // Player structure
 typedef struct Player{
@@ -49,25 +65,70 @@ typedef struct Brick {
     bool active;
 } Brick;
 
+// code to reset the bricks on reset or loss
+void resetBricks(Brick bricks[BRICKS_LINES][BRICKS_PER_LINE], int screenWidth)
+{
+    for (int j = 0; j < BRICKS_LINES; j++)
+    {
+        for (int i = 0; i < BRICKS_PER_LINE; i++)
+        {
+            bricks[j][i].size = (Vector2){ screenWidth/BRICKS_PER_LINE, 20 };
+            bricks[j][i].position = (Vector2){ i*bricks[j][i].size.x, j*bricks[j][i].size.y + BRICKS_POSITION_Y };
+            bricks[j][i].bounds = (Rectangle){ bricks[j][i].position.x, bricks[j][i].position.y, bricks[j][i].size.x, bricks[j][i].size.y };
+            bricks[j][i].active = true;
+        }
+    }
+}
 
+// check if all bricks have been destroyed
+bool allBricksDestroyed(Brick bricks[BRICKS_LINES][BRICKS_PER_LINE]) {
+    for (int j = 0; j < BRICKS_LINES; j++) {
+        for (int i = 0; i < BRICKS_PER_LINE; i++) {
+            if (bricks[j][i].active) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
+//create level complete bool to fix a bug when we reset the bricks.
+bool levelComplete = false;
+
+// VMU STUFF
+static uint8 vmu_lcd_bitmap[192];
+static uint8 vmu_lcd_bitmap2[192];
+int score = 0;
+int highScore = 0;
 
 // Program main entry point
 int main(void)
 {
+
     //Initialize Cool Variables and other such things
     const int screenWidth = ATTR_DRMCST_WIDTH;
     const int screenHeight = ATTR_DRMCST_HEIGHT;
-    SetConfigFlags(FLAG_MSAA_4X_HINT); //SetConfigFlags lets you set special rules in initialization. This currently MSAA at 4x.
+    SetConfigFlags(FLAG_MSAA_4X_HINT); //I don't know if anti aliasing is needed here
+
+    // Initialize KOS sound system
+    snd_init();
+    uint8_t volume = 128; // TODO this might fix sound, if not delete
 
     // Window Initialization
     InitWindow(screenWidth, screenHeight, "Attempting to make a game :D" );
 
     // load textures
-    Texture2D texLogo = LoadTexture("rd/rayliblogo.png");
+    Texture2D texRayLogo = LoadTexture("rd/rayliblogo.png");
+    Texture2D texLogo = LoadTexture("rd/madebycyp.png");
+
+    // load wav files found in romdisk
+    sfxhnd_t explosion = snd_sfx_load("/rd/explosion.wav");
+    sfxhnd_t bounce = snd_sfx_load("/rd/bounce.wav");
+    sfxhnd_t start = snd_sfx_load("/rd/start.wav");
 
 
     // Game variables
+    int level = 1; // sets level to 1
     int framesCounter = 0;  // Counts frames
     GameScreen screen = RAYLOGO; // Setting the starting screen
     int gameResult = -1;    // 0 = lose, 1 = win, -1 = not defined
@@ -103,11 +164,13 @@ int main(void)
             bricks[j][i].active = true;
         }
     }
-
-
-
-
-
+    // STUFF FOR VMU I DONT UNDERSTAND
+    //TODO DIE
+    maple_device_t * dev;
+    dev = maple_enum_type (0, MAPLE_FUNC_LCD);
+    load_bmp_vmu(vmu_lcd_bitmap, "/rd/ballbuster1.bmp");
+    load_bmp_vmu(vmu_lcd_bitmap2, "/rd/ballbuster2.bmp");
+    vmu_draw_lcd (dev, vmu_lcd_bitmap);
     //Initialization over
 
 
@@ -125,6 +188,8 @@ int main(void)
 
                 switch(screen)
                 {
+
+
                     case RAYLOGO:
                     {
                         // Update LOGO screen data here!
@@ -133,18 +198,19 @@ int main(void)
 
                         if (framesCounter > 180)
                         {
-                            screen = LOGO;    // Change to TITLE screen after 3 seconds
+                            screen = LOGO;    // Change to LOGO screen after 3 seconds
                             framesCounter = 0;
                         }
 
                     } break;
+
                     case LOGO:
                     {
                         // Update LOGO screen data here!
 
                         framesCounter++;
 
-                        if (framesCounter > 180)
+                        if (framesCounter > 240)
                         {
                             screen = TITLE;    // Change to TITLE screen after 3 seconds
                             framesCounter = 0;
@@ -152,15 +218,19 @@ int main(void)
 
                     } break;
 
-
-                    case TITLE:
+                    case TITLE:  // TODO - Make a real title screen
                     {
                         // Update TITLE screen data here!
 
                         framesCounter++;
 
-                        // LESSON 03: Inputs management (keyboard, mouse)
-                        if (IsGamepadButtonReleased(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT)) screen = GAMEPLAY;
+                        if (IsGamepadButtonReleased(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT))
+                        {
+                            snd_sfx_play_chn(1, start, 170, CENTER);
+
+
+                            screen = GAMEPLAY;
+                        }
 
                     } break;
                     case GAMEPLAY:
@@ -179,6 +249,21 @@ int main(void)
 
                                     player.bounds = (Rectangle){player.position.x, player.position.y, player.size.x, player.size.y };
 
+                                    //if all bricks are destroyed, go to next level
+                                    if (allBricksDestroyed(bricks)) {
+
+                                        ball.position.x = player.position.x + player.size.x/2;
+                                        ball.position.y = player.position.y - ball.radius - 1.0f;
+                                        ball.speed = (Vector2){ 0, 0 };
+                                        ball.active = false;
+                                        // TODO add a fancy animation, change color on level change
+                                        resetBricks(bricks, screenWidth);
+                                        bool levelComplete = true;
+                                        level++;
+
+                                    }
+
+
                                     if (ball.active)
                                     {
                                         // Ball movement logic
@@ -189,13 +274,12 @@ int main(void)
                                         if (((ball.position.x + ball.radius) >= screenWidth) || ((ball.position.x - ball.radius) <= 0)) ball.speed.x *= -1;
                                         if ((ball.position.y - ball.radius) <= 0) ball.speed.y *= -1;
 
-                                        // TODO: collision detection and resolution
-
                                         // Collision logic: ball vs player
                                         if (CheckCollisionCircleRec(ball.position, ball.radius, player.bounds))
                                         {
                                             ball.speed.y *= -1;
                                             ball.speed.x = (ball.position.x - (player.position.x + player.size.x/2))/player.size.x*5.0f;
+                                            snd_sfx_play_chn(1, bounce, 130, CENTER);
                                         }
                                         // Collision logic: ball vs bricks
                                         for (int j = 0; j <BRICKS_LINES; j++)
@@ -206,6 +290,8 @@ int main(void)
                                                 {
                                                     bricks[j][i].active = false;
                                                     ball.speed.y *= -1;
+                                                    snd_sfx_play_chn(1, bounce, 130, CENTER);
+                                                    score++;
 
                                                     break;
                                                 }
@@ -223,12 +309,25 @@ int main(void)
                                             ball.position.y = player.position.y - ball.radius - 1.0f;
                                             ball.speed = (Vector2){ 0, 0 };
                                             ball.active = false;
-
-                                            player.lifes--;
+                                            snd_sfx_play_chn(1, explosion, 130, CENTER);
+                                            if (levelComplete == false)
+                                            {
+                                                player.lifes--;
+                                            }
+                                            else
+                                            {
+                                                levelComplete = false;
+                                            }
                                         }
                                         if (player.lifes <0)
                                         {
+                                            resetBricks(bricks, screenWidth);
+                                            if (score > highScore)
+                                            {
+                                                highScore = score;
+                                            }
                                             screen = ENDING;
+                                            int level = 1;
                                             player.lifes = 5;
                                             framesCounter = 0;
                                         }
@@ -248,21 +347,26 @@ int main(void)
                                 }
 
 
-                        // if (!gamePaused)
-                        // {
-                            // TODO: Gameplay logic
-                        // }
+                         if (!gamePaused)
+                         {
+                            // TODO: make logic for a real pause menu
+                         }
 
 
                     } break;
-                    case ENDING:
+                    case ENDING: //TODO Make logic for ending screen
                     {
                         // Update END screen data here!
 
                         framesCounter++;
 
                         // LESSON 03: Inputs management (keyboard, mouse)
-                        if (IsGamepadButtonReleased(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT)) screen = TITLE;
+                        if (IsGamepadButtonReleased(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT)) {
+                            score = 0;
+                            screen = TITLE;
+                            snd_sfx_play_chn(1, start, 170, CENTER);
+
+                        }
 
                     } break;
                     default: break;
@@ -280,15 +384,16 @@ int main(void)
                     case RAYLOGO:
                     {
                         // TODO: Fix color!
-                        DrawTexture(texLogo, screenWidth/2 - texLogo.width/2, screenHeight/2 - texLogo.height/2, WHITE);
+                        DrawTexture(texRayLogo, screenWidth/2 - texLogo.width/2, screenHeight/2 - texLogo.height/2, WHITE);
                     } break;
+
                     case LOGO:
 
                     {
-                        // Draw LOGO screen here!
+                        DrawTexture(texLogo, screenWidth/2 - texLogo.width/2, screenHeight/2 - texLogo.height/2, WHITE);
+                    } break;
 
-                        DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-                    }
+
                     case TITLE:
                     {
                         // TODO: Draw TITLE screen here!
@@ -297,9 +402,10 @@ int main(void)
                         DrawText("PRESS START to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
 
                     } break;
+
                     case GAMEPLAY:
                     {
-                        // TODO: Draw GAMEPLAY screen here!
+
                         DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
                         // Draw basic shapes
                         DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, BLACK); //Draws players bar
@@ -318,25 +424,34 @@ int main(void)
 
                             }
 
-
-
                         }
 
                         // Draw GUI: Player lives
-                        for (int i = 0; i < player.lifes; i++) DrawRectangle(20 + 40*i, screenHeight -42, 35, 20, LIGHTGRAY);
+                        for (int i = 0; i < player.lifes; i++) DrawRectangle(20 + 40*i, screenHeight - 460, 35, 25, GREEN);
+                        // Draw GUI: Level
+
+                        DrawText(TextFormat("Level - %d", level), screenWidth - MeasureText(TextFormat("Level %d", level), 30) - 300, 20, 30, BLACK);
+                        // Draw GUI: Score
+
+                        DrawText(TextFormat("Score - %d", score), screenWidth - MeasureText(TextFormat("Score - %d", score), 30) - 80, 20, 30, BLACK);
+
 
                         // Draw pause message when start is pressed
                         if (gamePaused) DrawText("Paused!", screenWidth/2 - MeasureText("Paused!", 40)/2, screenHeight/2 + 60, 40, GRAY);
 
                     } break;
+
                     case ENDING:
                     {
                         // TODO: Draw ENDING screen here!
                         DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-                        DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                        DrawText("PRESS START to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+                        DrawText(TextFormat("Score - %d", score), (screenWidth / 2) - (MeasureText(TextFormat("Score - %d", score), 30) / 2), (screenHeight / 2) - 40, 30, BLACK);
+                        DrawText(TextFormat("High Score - %d", highScore), (screenWidth / 2) - (MeasureText(TextFormat("High Score - %d", highScore), 30) / 2), (screenHeight / 2) + 10, 30, BLACK);
+
+                        DrawText("PRESS START to RETURN to TITLE SCREEN", 90, 360, 20, DARKBLUE);
 
                     } break;
+
                     default: break;
                 }
                 // End of Switch (screen)
